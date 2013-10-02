72a2928a019121df23cfc09e2c543a401361914a
Julian Orth <ju.orth@gmail.com>
2013-10-03 00:13:18 +0200

    Show all tracks when artist is selected
    - If the artist is selected, all tracks are shown with headers indicating
    the albums.
    - There is an option called auto_expand_albums. If it's set to false, the
    player doesn't automatically select the album when commands such as
    win-sel-current are executed and instead selects the artist. If the
    albums of this artist are already expanded, the album is selected.

--- options.c
+++ options.c
@@ -77,6 +77,8 @@ int smart_artist_sort = 1;
 int scroll_offset = 2;
 int rewind_offset = 5;
 int skip_track_info = 0;
+int auto_expand_albums = 1;
+int show_all_tracks = 1;
 
 int colors[NR_COLORS] = {
 	-1,
@@ -873,6 +875,40 @@ static void toggle_show_hidden(unsigned int id)
 	browser_reload();
 }
 
+static void get_auto_expand_albums(unsigned int id, char *buf)
+{
+	strcpy(buf, bool_names[auto_expand_albums]);
+}
+
+static void set_auto_expand_albums(unsigned int id, const char *buf)
+{
+	parse_bool(buf, &auto_expand_albums);
+}
+
+static void toggle_auto_expand_albums(unsigned int id)
+{
+	auto_expand_albums ^= 1;
+}
+
+static void get_show_all_tracks(unsigned int id, char *buf)
+{
+	strcpy(buf, bool_names[show_all_tracks]);
+}
+
+static void set_show_all_tracks(unsigned int id, const char *buf)
+{
+	int old = show_all_tracks;
+	parse_bool(buf, &show_all_tracks);
+	if (old != show_all_tracks)
+		tree_sel_update(0);
+}
+
+static void toggle_show_all_tracks(unsigned int id)
+{
+	show_all_tracks ^= 1;
+	tree_sel_update(0);
+}
+
 static void get_show_current_bitrate(unsigned int id, char *buf)
 {
 	strcpy(buf, bool_names[show_current_bitrate]);
@@ -1215,6 +1251,8 @@ static const struct {
 	DN(replaygain_preamp)
 	DT(resume)
 	DT(show_hidden)
+	DT(auto_expand_albums)
+	DT(show_all_tracks)
 	DT(show_current_bitrate)
 	DT(show_playback_position)
 	DT(show_remaining_time)
